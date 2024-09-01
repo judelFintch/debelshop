3be42ab4fa3ec34caaf5d3fb5461633cea25cb72
Judel Fintch <judfintch@gmail.com>
2024-09-01 14:36:09 +0200

    feat :reduce number pagination

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -14,7 +14,7 @@ class Products extends Component
     public function render(): View
     {
         return view('livewire.products', [
-            'products' => Product::paginate(8)
+            'products' => Product::paginate(4)
         ]);
     }
 }
