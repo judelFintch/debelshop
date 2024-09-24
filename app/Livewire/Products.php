2297eab93da9e75af480b311ee735b07a222eb22
Judel Fintch <judfintch@gmail.com>
2024-09-24 17:31:24 +0200

    feat :adding picutes

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -3,7 +3,6 @@
 namespace App\Livewire;
 
 use App\Models\Product;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
@@ -11,10 +10,39 @@ use Livewire\Component;
 #[Layout('layouts.app')]
 class Products extends Component
 {
+    public function mount()
+    {
+        $this->updateProducts();
+    }
+
+    public function updateProducts()
+    {
+        $updates = [
+            1 => [
+                'title' => 'Noah 4 cylindres Essence',
+                'price' => 5000,
+            ],
+            2 => [
+                'title' => 'Alphard 2002',
+                'price' => 3.000,
+            ],
+        ];
+
+        foreach ($updates as $id => $data) {
+            $product = Product::find($id);
+            if ($product) {
+                $product->title = $data['title'];
+                $product->price = $data['price'];
+                $product->save(); // Enregistre les modifications
+            }
+        }
+    }
+
     public function render(): View
     {
         return view('livewire.products', [
-            'products' => Product::paginate(4)
+            'products' => Product::paginate(2)
         ]);
     }
 }
+
--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -25,7 +25,7 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 pt-16">
             @foreach($products as $product)
                 <div class="relative">
-                    <img src="{{ asset('img/banner.jpg') }}" class="h-[400px] object-cover object-center rounded-xl shadow-sm border border-gray-200" alt="">
+                    <img src="{{ asset('img/' . $product->id . '.jpg') }}" class="h-[400px] object-cover object-center rounded-xl shadow-sm border border-gray-200" alt="">
                     <div class="absolute inset-0 p-4 flex flex-col justify-between">
                         <div>
                             <span class="bg-red-400 px-4 py-2 rounded-lg backdrop-blur-lg text-sm text-white">30% OFF</span>
@@ -111,9 +111,9 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 pt-16">
             @foreach($products as $product)
                 <div class="relative">
-                    <img src="{{ asset('img/banner.jpg') }}" class="h-[300px] object-cover object-center rounded-xl shadow-sm border border-gray-200" alt="">
+                    <img src="{{ asset('img/' . $product->id . '.jpg') }}" class="h-[300px] object-cover object-center rounded-xl shadow-sm border border-gray-200" alt="">
                     <a href="#" class="absolute inset-0 flex justify-end px-4 py-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                        <svg xmlns="" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
                     </a>
