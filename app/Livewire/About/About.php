6b0de261bef738e7960fed055a005eceb9d921e3
Judel Fintch <judfintch@gmail.com>
2024-10-08 15:16:41 +0200

    feat :adding layout

--- app/Livewire/About/About.php
+++ app/Livewire/About/About.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\About;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class About extends Component
 {
+    #[Layout('layouts.app')]
     public function render()
     {
         return view('livewire.about.about');
--- app/Livewire/Contact/Contact.php
+++ app/Livewire/Contact/Contact.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Contact;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Contact extends Component
 {
+    #[Layout('layouts.app')]
     public function render()
     {
         return view('livewire.contact.contact');
